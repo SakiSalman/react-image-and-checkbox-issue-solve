5e3b5b757ffba204df858a7f0d786d249ad522b8
Saki Salman <codersaki98@gmail.com>
2023-02-05 20:58:03 +0600

    Image upload and Checkbox upload solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Upload from "./component/Upload/Upload";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Checkbox from "./component/checkbox/Checkbox";
 
 function App() {
-  return <Upload></Upload>;
+  return <Checkbox></Checkbox>;
 }
 
 export default App;
